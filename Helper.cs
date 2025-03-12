2cd151b7d728e8b2acaaca92c95733e5a6374d4c
beemerwt <beemerwt@gmail.com>
2025-03-12 12:42:13 -0500

    Use location instance for tracking

--- Helper.cs
+++ Helper.cs
@@ -1,46 +1,62 @@
-﻿using System.Reflection;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
 namespace RuneTablet
 {
-    public static class Helper
+	public static class Helper
     {
-        public static Sprite GetSprite(string fileName)
-        {
-            // Read the image from the embedded resource into a byte array
-            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName);
-
-            byte[] imageBytes = new byte[stream.Length];
-            stream.Read(imageBytes, 0, imageBytes.Length);
-
-            // Create a new texture and load the image data into it
-            Texture2D tex = new(1, 1);
-            tex.LoadImage(imageBytes);
-
-            // Create a new sprite from the texture
-            return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-        }
-
-        public static string GetHashedName(MonoBehaviour component)
+        public static string GetHashedName(RuneStone component)
         {
             var position = component.transform.position;
             var hash = ((int)position.x * 31) + ((int)position.z * 37);
             return component.name + "-" + hash.ToString();
         }
-        public static string GetPinName(string locationName)
-        {
-            return locationName switch {
-                "Crypt2" or "Crypt3" or "Crypt4" => "Burial Chambers",
-                "Mistlands_DvergrTownEntrance1" or "Mistlands_DvergrTownEntrance2" => "Infested Mine",
-                "Hildir_crypt" => "Smouldering Tomb",
-                "Hildir_cave" => "Howling Cavern",
-                "Hildir_plainsfortress" => "Sealed Tower",
-                "MountainCave02" => "Frost Caves",
-                "GoblinCamp2" => "Goblin Camp",
-                "SunkenCrypt4" => "Sunken Crypt",
-                "StoneTower1" or "StoneTower3" => "Stone Tower",
-                _ => "Dungeon",
-            };
-        }
-    }
+
+		public static ZoneSystem.LocationInstance? GetLocationInstance(Location location)
+		{
+			var zoneId = ZoneSystem.GetZone(location.transform.position);
+			if (!ZoneSystem.instance.m_locationInstances.TryGetValue(zoneId, out ZoneSystem.LocationInstance instance))
+				return null;
+
+			return instance;
+		}
+
+		public static string GetHashedName(Location location)
+		{
+			var instance = GetLocationInstance(location);
+			if (instance.HasValue)
+				return GetHashedName(instance.Value);
+
+			Plugin.Logger.LogError("Could not find location instance for " + location.name);
+			return null;
+		}
+
+		public static string GetHashedName(ZoneSystem.LocationInstance instance)
+		{
+			var zoneId = ZoneSystem.GetZone(instance.m_position);
+			var worldName = ZNet.World.m_name;
+			var prefabName = instance.m_location.m_prefab.Name;
+			var hash = (zoneId.x * 31) + (zoneId.y * 37);
+			return $"{worldName}-{prefabName}-{hash}";
+		}
+
+		private static Dictionary<string, string> PinNames = new() {
+			{ "Crypt2", "$location_forestcrypt" },
+			{ "Crypt3", "$location_forestcrypt" },
+			{ "Crypt4", "$location_forestcrypt" },
+			{ "Mistlands_DvergrTownEntrance1", "$location_dvergrtown" },
+			{ "Mistlands_DvergrTownEntrance2", "$location_dvergrtown" },
+			{ "Hildir_crypt", "$hud_pin_hildir1" },
+			{ "Hildir_cave", "$hud_pin_hildir2" },
+			{ "Hildir_plainsfortress", "$hud_pin_hildir3" },
+			{ "MountainCave02", "$location_mountaincave" },
+			{ "GoblinCamp2", "$enemy_goblin" },
+			{ "SunkenCrypt4", "$location_sunkencrypt" },
+			{ "MorgenHole1", "$location_morgenhole" },
+			{ "MorgenHole2", "$location_morgenhole" },
+			{ "MorgenHole3", "$location_morgenhole" },
+		};
+
+		public static string GetPinName(string locationName)
+			=> PinNames.TryGetValue(locationName, out string name) ? name : locationName;
+	}
 }
---
+++ Teleport.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace RuneTablet
+{
+	[HarmonyPatch(typeof(Teleport), nameof(Teleport.Interact))]
+	public class Teleport_Interact_Patch
+	{
+		private static void Postfix(Humanoid character, Teleport __instance, ref bool __result)
+		{
+			// We didn't enter...
+			if (!__result || !character.InInterior()) return;
+
+			var otherSide = __instance.m_targetPoint; // ExteriorGateway
+			var dungeon = otherSide.GetComponentInParent<Location>();
+			if (dungeon == null) return;
+
+			var locationInstance = Helper.GetLocationInstance(dungeon);
+			if (locationInstance.HasValue)
+				Plugin.SaveLocation(locationInstance.Value);
+			else
+				Plugin.Logger.LogError("Could not find location instance for " + dungeon.name);
+		}
+	}
+}
