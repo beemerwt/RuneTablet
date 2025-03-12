da319759d193055b0f4f9816afa9f48d82813ca7
beemerwt <beemerwt@gmail.com>
2025-03-12 12:52:47 -0500

    more linter

--- RuneCrystal.cs
+++ RuneCrystal.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 namespace RuneTablet
 {
-    public class RuneCrystal : CustomItem
-    {
-        internal static Sprite IconSprite;
+	public class RuneCrystal : CustomItem
+	{
+		internal static Sprite IconSprite;
 
-        public RuneCrystal() : base("RuneCrystal", true, new ItemConfig() {
-            Name = "$item_runecrystal",
-            Description = "$item_runecrystal_desc",
-            StackSize = 10,
-            Weight = 1,
-            Icon = IconSprite,
-        }) { }
-    }
+		public RuneCrystal() : base("RuneCrystal", true, new ItemConfig() {
+			Name = "$item_runecrystal",
+			Description = "$item_runecrystal_desc",
+			StackSize = 10,
+			Weight = 1,
+			Icon = IconSprite,
+		})
+		{ }
+	}
 }
--- RuneStone.cs
+++ RuneStone.cs
@@ -3,27 +3,27 @@ using Jotunn.Managers;
 
 namespace RuneTablet
 {
-    [HarmonyPatch(typeof(RuneStone), nameof(RuneStone.Interact))]
-    public class RuneStoneInteractPatch
-    {
-        public static void Postfix(Humanoid character, bool hold, bool alt, RuneStone __instance)
-        {
-            Plugin.Logger.LogInfo("RuneStone.Interact: Called");
-            var name = Helper.GetHashedName(__instance);
+	[HarmonyPatch(typeof(RuneStone), nameof(RuneStone.Interact))]
+	public class RuneStoneInteractPatch
+	{
+		public static void Postfix(Humanoid character, bool hold, bool alt, RuneStone __instance)
+		{
+			Plugin.Logger.LogInfo("RuneStone.Interact: Called");
+			var name = Helper.GetHashedName(__instance);
 
-            Plugin.Logger.LogInfo("RuneStone.Interact: Found runestone " + name);
+			Plugin.Logger.LogInfo("RuneStone.Interact: Found runestone " + name);
 
-            if (Plugin.FoundRuneStones.Contains(name))
-                return;
+			if (Plugin.FoundRuneStones.Contains(name))
+				return;
 
-            // Spawn a rune crystal for the player
-            var crystalPrefab = PrefabManager.Instance.GetPrefab("RuneCrystal");
-            if (character.m_inventory.CanAddItem(crystalPrefab))
-            {
-                Player.m_localPlayer.m_inventory.AddItem(crystalPrefab, 1);
-                Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, "A mysterious crystal falls into your hand.");
-                Plugin.FoundRuneStones.Add(name);
-            }
-        }
-    }
+			// Spawn a rune crystal for the player
+			var crystalPrefab = PrefabManager.Instance.GetPrefab("RuneCrystal");
+			if (character.m_inventory.CanAddItem(crystalPrefab))
+			{
+				Player.m_localPlayer.m_inventory.AddItem(crystalPrefab, 1);
+				Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, "A mysterious crystal falls into your hand.");
+				Plugin.FoundRuneStones.Add(name);
+			}
+		}
+	}
 }
